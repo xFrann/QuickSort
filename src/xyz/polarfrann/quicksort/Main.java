8d0053b973f0754324d76fad55eae44c590727fa
xFrann <madalinxd22@gmail.com>
2020-06-05 14:13:48 +0300

    Implemented user I/O methods

--- src/xyz/polarfrann/quicksort/Main.java
+++ src/xyz/polarfrann/quicksort/Main.java
@@ -3,22 +3,46 @@ package xyz.polarfrann.quicksort;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
         Quicksort qs = new Quicksort();
 
+        InputHandler input = new InputHandler();
+
+        System.out.print("How many integers should the array contain?: ");
+        int size = input.scanSize();
+
+
+        System.out.println("Please insert the minimum and maximum values of the integers: ");
+
+        int min = input.getMinimumValue();
+        int max = input.getMaximumValue();
+
+
         //Create a random array to be sorted
-        int[] arrayToSort = new RandomArray().createBetween(10000, 1, 1000);
+        int[] arrayToSort = new RandomArray().createBetween(size, min, max);
 
-        //Start the timer for the duration of sorting the elements
-        long start = System.currentTimeMillis();
-        ElapsedTimeCalculator timeCalculator = new ElapsedTimeCalculator(start);
+        System.out.print("Do you want to visualize the sorting time duration of the algorithm?: ");
+        if (input.showElapsedTime()) {
 
-        //Sort the array and show the results
-        qs.quickSort(arrayToSort, 0, arrayToSort.length - 1);
-        qs.showResult(arrayToSort, timeCalculator.calculateElapsedTime());
+            //Start the timer for the duration of sorting the elements
+            long start = System.currentTimeMillis();
 
+            //Sort the array
+            qs.quickSort(arrayToSort, 0, arrayToSort.length - 1);
+
+            //Stop the timer and show the result
+            ElapsedTimeCalculator timeCalculator = new ElapsedTimeCalculator(start);
+            qs.showResult(arrayToSort, timeCalculator.calculateElapsedTime());
+
+        } else {
+
+            //Sort the array && Show the result
+            qs.quickSort(arrayToSort, 0, arrayToSort.length - 1);
+
+            qs.showResult(arrayToSort);
+
+        }
     }
 
 }
