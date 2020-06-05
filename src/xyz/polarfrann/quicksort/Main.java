38166eac5ddeabf188c8062d503d29953fd5bffa
xFrann <madalinxd22@gmail.com>
2020-06-05 13:01:09 +0300

    Added missing parameter on showResult() method, changed size of the Array

--- src/xyz/polarfrann/quicksort/Main.java
+++ src/xyz/polarfrann/quicksort/Main.java
@@ -3,14 +3,22 @@ package xyz.polarfrann.quicksort;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
         Quicksort qs = new Quicksort();
 
         //Create a random array to be sorted
-        int[] arrayToSort = new RandomArray().createBetween(50, 1, 100);
+        int[] arrayToSort = new RandomArray().createBetween(10000, 1, 1000);
+
+        //Start the timer for the duration of sorting the elements
+        long start = System.currentTimeMillis();
+        ElapsedTimeCalculator timeCalculator = new ElapsedTimeCalculator(start);
+
+        //Sort the array and show the results
         qs.quickSort(arrayToSort, 0, arrayToSort.length - 1);
-        qs.showResult(arrayToSort);
+        qs.showResult(arrayToSort, timeCalculator.calculateElapsedTime());
+
     }
 
 }
