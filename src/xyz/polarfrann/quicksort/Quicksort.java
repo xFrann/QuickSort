574d9bf624310b069d7a27e8a2566043f5701bdd
xFrann <madalinxd22@gmail.com>
2020-06-05 11:01:03 +0300

    Added a main quicksort and result printing methods

--- src/xyz/polarfrann/quicksort/Quicksort.java
+++ src/xyz/polarfrann/quicksort/Quicksort.java
@@ -3,5 +3,19 @@ package xyz.polarfrann.quicksort;
 public class Quicksort {
 
 
+    //All sorting logic inside this method
+    //@param big - hold the value of the last element in the array, the pivot
+    //@param small - variable that holds a element smaller than the pivot
+    public void quickSort(int[] array, int small, int big) {
 
+    }
+
+    //This method will print the sorted array and
+    //the time that it took to sort it.
+    private void showResult(int[] array) {
+        System.out.println("Elements sorted in just: " + " ms");
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+    }
 }
