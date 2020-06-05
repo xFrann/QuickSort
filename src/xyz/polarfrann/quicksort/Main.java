324199c63cbf5298b7a7d8a56ab5274a546422a0
xFrann <madalinxd22@gmail.com>
2020-06-05 12:45:30 +0300

    Generated, sorted and printed an array

--- src/xyz/polarfrann/quicksort/Main.java
+++ src/xyz/polarfrann/quicksort/Main.java
@@ -1,12 +1,16 @@
 package xyz.polarfrann.quicksort;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
         Quicksort qs = new Quicksort();
 
-
+        //Create a random array to be sorted
+        int[] arrayToSort = new RandomArray().createBetween(50, 1, 100);
+        qs.quickSort(arrayToSort, 0, arrayToSort.length - 1);
+        qs.showResult(arrayToSort);
     }
 
 }
